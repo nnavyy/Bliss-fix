2b8740fe6aea02e95e430221278c8dd76c7e3f73
nnavyy <nandazhafran@gmail.com>
2025-12-24 18:04:25 +0700

    1.11.38 update env

--- frontend/app/lib/prisma.ts
+++ frontend/app/lib/prisma.ts
@@ -1,17 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
-};
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query", "error"],
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+const prisma = new PrismaClient().$extends(withAccelerate());
 
 export default prisma;
