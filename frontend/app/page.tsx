0923415f8b220531fbba48b5a2edd6fa267e1741
nnavyy <nandazhafran@gmail.com>
2025-12-27 22:35:39 +0700

    1.11.48 all working and update page tsx forntend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,28 @@
-export default function Page() {
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/login");
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
-    <main>
-      <h1>Next.js</h1>
-      <p>dont go</p>
-    </main>
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-xl font-semibold">Masuk ke Medikidney</h1>
+        <p className="text-gray-500 mt-2">Loading...</p>
+
+        {/* spinner */}
+        <div className="mt-4 h-8 w-8 animate-spin rounded-full border-4 border-gray-300 border-t-black mx-auto" />
+      </div>
+    </div>
   );
 }
