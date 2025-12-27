a700654b96220cc62ed60a64ce34210bb7161851
nnavyy <nandazhafran@gmail.com>
2025-12-27 21:41:46 +0700

    1.11.42 update route

--- frontend/app/api/auth/login/route.ts
+++ frontend/app/api/auth/login/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
-import prisma from "@/app/lib/prisma";
+import{ prisma }from "@/app/lib/prisma";
 
 export async function POST(req: Request) {
   try {
